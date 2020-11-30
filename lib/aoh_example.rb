fa696fb31eea6fe0fe5dd8afba34416ec78645ac
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-11-30 18:38:48 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -30,6 +30,12 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
+  aoh = [{:name=>"Don Gately", :occupation=>"Live-in Staff"}, 
+        {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"}, 
+        { :name=>  "Pat Monteseian", occupation: "Staff" },
+        {:name=>"Kate Gompert", :occupation=>"None"}, 
+        {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
+    ]
 end
 
 def aoh_update(aoh, row, key, new_value)
